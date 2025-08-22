3ac674b75c04b4338a67f6fd5d0c91194da95b06
1 <ypopov2005@gmail.com>
2025-08-22 14:22:31 +0300

    update

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import os
 from typing import Optional
 from sqlalchemy import select
 from app.database import async_sesion_maker
-from app.students.models import  Student, RBStudent, SUpdateFilter, StudentUpdate, SDeleteFilter
+from app.students.models import Student
+# from app.students.models import  Student, RBStudent, SUpdateFilter, StudentUpdate, SDeleteFilter
 
 
 
@@ -28,9 +29,9 @@ def get_all_students():
     # return JSONResponse(students)
 
 
-@app.get("/students/{course}")
-def get_all_students_course(request_body: RBStudent = Depends()) -> list[Student]:
-    pass
+# @app.get("/students/{course}")
+# def get_all_students_course(request_body: RBStudent = Depends()) -> list[Student]:
+#     pass
     # students = json_to_dict_list()
 
     # filtered_students = []
@@ -50,18 +51,18 @@ def get_all_students_course(request_body: RBStudent = Depends()) -> list[Student
     # return filtered_students
 
 
-@app.get("/student", response_model=Student)
-def get_student_by_id(student_id: int):
-    pass
+# @app.get("/student", response_model=Student)
+# def get_student_by_id(student_id: int):
+#     pass
     # students = json_to_dict_list()
     # for student in students:
     #     if student["student_id"] == student_id:
     #         return student
 
 
-@app.post("/add_student")
-def add_student_handler(student: Student):
-    pass
+# @app.post("/add_student")
+# def add_student_handler(student: Student):
+#     pass
     # student_dict = student.dict()
     # check = add_student(student_dict)
 
@@ -71,22 +72,22 @@ def add_student_handler(student: Student):
     #     return JSONResponse({"message": "Ошибка про добавлении студента"})
 
 
-@app.put("/update_student")
-def update_student_handler(filter_sudent: SUpdateFilter, new_data: StudentUpdate):
-    pass
-    # check = upd_student(filter_sudent.dict(), new_data.dict())
+# @app.put("/update_student")
+# def update_student_handler(filter_sudent: SUpdateFilter, new_data: StudentUpdate):
+#     pass
+#     # check = upd_student(filter_sudent.dict(), new_data.dict())
 
-    # if check:
-    #     return JSONResponse({"message" : "Информация успешно обновлена!"})
-    # else:
-    #     raise HTTPException(status_code=4000, detail="Ошика при обновлении информации о студенте")
+#     # if check:
+#     #     return JSONResponse({"message" : "Информация успешно обновлена!"})
+#     # else:
+#     #     raise HTTPException(status_code=4000, detail="Ошика при обновлении информации о студенте")
     
-@app.delete("/delete_student")
-def delete_student_handler(filter_student: SDeleteFilter):
-    pass
-    # check = dell_student(filter_student.key, filter_student.value)
+# @app.delete("/delete_student")
+# def delete_student_handler(filter_student: SDeleteFilter):
+#     pass
+#     # check = dell_student(filter_student.key, filter_student.value)
 
-    # if check:
-    #     return JSONResponse({"message" : "Студент успешно удален"} )
-    # else:
-    #     raise HTTPException(status_code=400, detail="Ошибка при удалении")
+#     # if check:
+#     #     return JSONResponse({"message" : "Студент успешно удален"} )
+#     # else:
+#     #     raise HTTPException(status_code=400, detail="Ошибка при удалении")
--- app/students/models.py
+++ app/students/models.py
@@ -1,74 +1,114 @@
 
 
 
-from enum import Enum
-from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError
-from datetime import date, datetime
-from typing import Optional, Any
-import re
+# from enum import Enum
+# from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError
+# from datetime import date, datetime
+# from typing import Optional, Any
+# import re
 
 
-class Major(str, Enum):
-    informatics = "Информатика"
-    economics = "Экономика"
-    low = "Право"
-    medicine = "Медицина"
-    engenering = "Инженерия"
-    languages = "Языки"
-    history = "История"
-    math = "Математика"
-    biology = "Биология"
+# class Major(str, Enum):
+#     informatics = "Информатика"
+#     economics = "Экономика"
+#     low = "Право"
+#     medicine = "Медицина"
+#     engenering = "Инженерия"
+#     languages = "Языки"
+#     history = "История"
+#     math = "Математика"
+#     biology = "Биология"
 
 
-class RBStudent:
-    def __init__(self, course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
-        self.course: int = course
-        self.major: Optional[str] = major
-        self.enrollment_year: Optional[int] = enrollment_year
+# class RBStudent:
+#     def __init__(self, course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
+#         self.course: int = course
+#         self.major: Optional[str] = major
+#         self.enrollment_year: Optional[int] = enrollment_year
 
-class SUpdateFilter(BaseModel):
-    student_id: int
+# class SUpdateFilter(BaseModel):
+#     student_id: int
 
-class SDeleteFilter(BaseModel):
-    key: str
-    value: Any
+# class SDeleteFilter(BaseModel):
+#     key: str
+#     value: Any
 
-class StudentUpdate(BaseModel):
-    course: int = Field(..., ge=1, le=5, description="Курс, от 1 до 5")
-    major: Optional[Major] = Field(..., description="Специальность")
+# class StudentUpdate(BaseModel):
+#     course: int = Field(..., ge=1, le=5, description="Курс, от 1 до 5")
+#     major: Optional[Major] = Field(..., description="Специальность")
 
-class Student(BaseModel):
-    student_id: int
-    phone_number: str = Field(default=..., description="Номер телефона с +")
-    first_name: str = Field(default=..., min_length=1,
-                            max_length=58, description="Имя студента")
-    last_name: str = Field(default=..., min_length=1,
-                           max_length=58, description="Фамилия студента")
-    date_of_birth: date = Field(
-        default=..., description="Дата рожления студента в формате от 1 до 50 символов")
-    email: EmailStr = Field(default=..., description="Электронная почта")
-    address: str = Field(default=..., min_length=1,
-                         max_length=200, description="Адрес студента")
-    enrollment_year: int = Field(default=..., description="год поступления")
-    major: Major = Field(default=..., description="Специальность студента")
-    course: int = Field(default=...,  ge=1, le=5,
-                        description="Курс должен быть в диапазоне от 1 до 5")
-    special_notes: Optional[str] = Field(
-        default=None, max_length=600, description="Дополнительные заметки до 600 символов")
-
-
-@field_validator("phone_number")
-@classmethod
-def validate_phone_number(cls, values: str) -> str:
-    if not re.match(r'^\+\d{1,15}$', values):
-        raise ValueError(
-            'Номер телефона должен начинаться с "+" и содержать от 1 до 15 цифр')
-    return values
+# class Student(BaseModel):
+#     student_id: int
+#     phone_number: str = Field(default=..., description="Номер телефона с +")
+#     first_name: str = Field(default=..., min_length=1,
+#                             max_length=58, description="Имя студента")
+#     last_name: str = Field(default=..., min_length=1,
+#                            max_length=58, description="Фамилия студента")
+#     date_of_birth: date = Field(
+#         default=..., description="Дата рожления студента в формате от 1 до 50 символов")
+#     email: EmailStr = Field(default=..., description="Электронная почта")
+#     address: str = Field(default=..., min_length=1,
+#                          max_length=200, description="Адрес студента")
+#     enrollment_year: int = Field(default=..., description="год поступления")
+#     major: Major = Field(default=..., description="Специальность студента")
+#     course: int = Field(default=...,  ge=1, le=5,
+#                         description="Курс должен быть в диапазоне от 1 до 5")
+#     special_notes: Optional[str] = Field(
+#         default=None, max_length=600, description="Дополнительные заметки до 600 символов")
 
 
-@field_validator("date_of_birth")
-@classmethod
-def validate_date_of_birth(cls, values: date):
-    if values and values >= datetime.now().date():
-        raise ValueError('Дата рождения должна быть в прошлом')
-    return values
+# @field_validator("phone_number")
+# @classmethod
+# def validate_phone_number(cls, values: str) -> str:
+#     if not re.match(r'^\+\d{1,15}$', values):
+#         raise ValueError(
+#             'Номер телефона должен начинаться с "+" и содержать от 1 до 15 цифр')
+#     return values
+
+
+# @field_validator("date_of_birth")
+# @classmethod
+# def validate_date_of_birth(cls, values: date):
+#     if values and values >= datetime.now().date():
+#         raise ValueError('Дата рождения должна быть в прошлом')
+#     return values
+
+from sqlalchemy import ForeignKey, text, Text
+from sqlalchemy.orm import relationship, Mapped, mapped_column 
+from app.database import Base, str_uniq, int_pk, str_null_true
+from datetime import date
+
+class Student(Base):
+    id: Mapped[int_pk]
+    phone_number: Mapped[str_uniq]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    date_of_birth: Mapped[date]
+    email: Mapped[str_uniq]
+    adress: Mapped[str]= mapped_column(Text, nullable=False)
+    enrollment_year: Mapped[int]
+    course: Mapped[int]
+    special_notes: Mapped[str_null_true]
+    major_id: Mapped[int] = mapped_column(ForeignKey("majors.id"), nullable=False)
+    # ForeignKey: это класс в SQLAlchemy, который позволяет создавать внешние ключи в базе данных. Внешний ключ — это ссылка на значение в другой таблице, который обеспечивает целостность данных.
+    major: Mapped["Major"] = relationship("Major", back_populates="atudents")
+
+    def __str__(self):
+        return (f"{self.__class__.__name__}(id={self.id}), "
+                f"first_name={self.first_name!r}",
+                f"last_name={self.last_name!r}")
+    
+    def __repr__(self):
+        return str(self)
+    
+class Major(Base):
+    id: Mapped[int_pk]
+    major_name: Mapped[str_uniq]
+    major_descripion: Mapped[str_null_true]
+    count_students: Mapped[int] = mapped_column(server_default=text('0'))
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}), major_name={self.major_name!r}"
+    
+    def __repr__(self):
+        return str(self)
